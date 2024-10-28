75144783bcbaa769660cebf7d783dacbfe8453bc
qingyingliu <2596736318@qq.com>
2024-10-28 23:58:30 +0800

    feat: 升级 gradle && 更新 demo

--- host/src/main/java/com/dexclassdemo/liuguangli/dexclassloaderdemo/MainActivity.java
+++ host/src/main/java/com/dexclassdemo/liuguangli/dexclassloaderdemo/MainActivity.java
@@ -2,7 +2,6 @@ package com.dexclassdemo.liuguangli.dexclassloaderdemo;
 
 
 import android.os.Bundle;
-import android.os.Environment;
 
 import android.support.v7.app.AppCompatActivity;
 
@@ -11,8 +10,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.dexclassdemo.liuguangli.R;
+
 import java.io.File;
 
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -20,47 +23,74 @@ import java.lang.reflect.Method;
 import dalvik.system.DexClassLoader;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String DIR_NAME = "plugins";
+    private static final String FILE_NAME = "bundle-debug.apk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            String apkPath = getApkFilePath();
+            loadApk(apkPath);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        String apkPath = Environment.getExternalStorageDirectory() + "/bundle.apk";
-        loadApk(apkPath);
     }
 
-    public  void loadApk(String apkPath) {
+    public String getApkFilePath() throws Exception {
+        File dir = new File(this.getFilesDir(), DIR_NAME);
+        File apkFile = new File(dir, FILE_NAME);
+        if (apkFile.exists()) {
+            return apkFile.getAbsolutePath();
+        }
+
+        // 确保 dir 目录存在
+        dir.mkdirs();
+
+        try (InputStream inputStream = this.getAssets().open(DIR_NAME + "/" + FILE_NAME)) {
+            try (FileOutputStream outputStream = new FileOutputStream(apkFile)) {
+                byte[] buffer = new byte[2048];
+                int len = -1;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, len);
+                }
+            }
+        }
+        return apkFile.getAbsolutePath();
+    }
+
+    public void loadApk(String apkPath) {
 
 
         Log.v("loadDexClasses", "Dex Preparing to loadDexClasses!");
 
         File dexOpt = this.getDir("dexOpt", MODE_PRIVATE);
-            final DexClassLoader classloader = new DexClassLoader(
-                    apkPath,
-                    dexOpt.getAbsolutePath(),
-                    null,
-                    this.getClassLoader());
+        final DexClassLoader classloader = new DexClassLoader(
+                apkPath,
+                dexOpt.getAbsolutePath(),
+                null,
+                this.getClassLoader());
 
-            Log.v("loadDexClasses", "Searching for class : "
-                    + "com.registry.Registry");
-            try {
-                Class<?> classToLoad = (Class<?>) classloader.loadClass("com.dexclassdemo.liuguangli.apkbeloaded.ClassToBeLoad");
-                Object instance = classToLoad.newInstance();
-                Method method = classToLoad.getMethod("method");
-                method.invoke(instance);
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (ClassNotFoundException e) {
-                e.printStackTrace();
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            }
+        Log.v("loadDexClasses", "Searching for class : "
+                + "com.registry.Registry");
+        try {
+            Class<?> classToLoad = (Class<?>) classloader.loadClass("com.dexclassdemo.liuguangli.apkbeloaded.ClassToBeLoad");
+            Object instance = classToLoad.newInstance();
+            Method method = classToLoad.getMethod("method");
+            method.invoke(instance);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
 
 
     }
